c0ba223a267d0630788ec0c78c4ef7cfc834b0b0
Moh Fatkhur Rahman <akufatkhur62@gmail.com>
2024-03-13 12:30:22 +0700

    migrations

--- database/migrations/2024_03_13_051828_create_mahasiwa_table.php
+++ database/migrations/2024_03_13_051828_create_mahasiwa_table.php
@@ -11,13 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->string('nim')->primary();
+            $table->string('nama');
+            $table->enum('jenis_kelamin', ['L', 'P']);
+            $table->string('kode_kelas');
             $table->timestamps();
         });
     }
@@ -27,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('mahasiwa');
     }
 };
