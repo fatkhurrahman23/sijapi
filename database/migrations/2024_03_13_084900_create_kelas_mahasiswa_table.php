fa52260cb0dd1bf79a5ab854feca63441f6076fa
Rais Hannan Rizanto <114925063+vierohanz@users.noreply.github.com>
2024-03-13 16:30:32 +0700

    nambah kelas mahasiswa

---
+++ database/migrations/2024_03_13_084900_create_kelas_mahasiswa_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('kelas_mahasiswa', function (Blueprint $table) {
+            $table->id();
+            $table->string(kode_kelas, 20);
+            $table->string('prodi', 20);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('kelas_mahasiswa');
+    }
+};
