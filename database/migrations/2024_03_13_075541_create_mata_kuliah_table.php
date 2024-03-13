77c61c9a7a0da1398776a01bcd92ef4617e0eb13
Rais Hannan Rizanto <114925063+vierohanz@users.noreply.github.com>
2024-03-13 15:31:02 +0700

    nambah table matkul

---
+++ database/migrations/2024_03_13_075541_create_mata_kuliah_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mata_kuliah', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode_matakuliah', 20);
+            $table->string('kode_kelas', 20);
+            $table->string('nama', 20);
+            $table->int('sks', 20);
+            $table->int('semester', 20);
+            $table->enum('jenis', ['gasal', 'genap']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mata_kuliah');
+    }
+};
