78d0ad06505036a769016f92b4f08d5cd30fc862
AuliaAzizah16 <auliaazizahemail@gmail.com>
2024-03-14 15:54:09 +0800

    C:\xampp\htdocs\sijapi\app\Models\Prodi_dosen.php

---
+++ app/Models/Prodi_dosen.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Prodi_dosen extends Model
+{
+    use HasFactory;
+}
