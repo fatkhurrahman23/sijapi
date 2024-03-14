2b44af61322efb8d200f7ef11d7eeba897ddce3d
Rais Hannan Rizanto <114925063+vierohanz@users.noreply.github.com>
2024-03-14 14:47:57 +0700

    users

---
+++ app/Models/Users.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Users extends Model
+{
+    use HasFactory;
+
+    protected $table = 'users';
+    protected $primarykey = 'id';
+    protected $fillable = ['id' ,'username', 'password', 'level'];
+
+    public function dosen(){
+        return $this->belongsto('app\Models\Dosen.php', 'id');
+    }
+}
