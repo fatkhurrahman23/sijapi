19df2d79f9c4fbeb63a366f3223a9fbe711f8001
AuliaAzizah16 <auliaazizahemail@gmail.com>
2024-03-14 14:50:52 +0800

    mmm

---
+++ app/Models/Ruang.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Factories\Model;
+
+class DataRuang extends Model
+{
+    use HasFactory;
+
+    protected $table = 'data_ruang';
+}
