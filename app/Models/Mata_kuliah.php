fca4fb56941d89927653d0980ca0232581d02532
AuliaAzizah16 <auliaazizahemail@gmail.com>
2024-03-14 17:06:50 +0800

    C:\xampp\htdocs\sijapi\app\Models\Mata_kuliah.php

--- app/Models/Mata_kuliah.php
+++ app/Models/Mata_kuliah.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Mata_kuliah extends Model
 {
     use HasFactory;
+
+    protected $table = 'mata_kuliah';
+
+    public function kelas_mahasiswa(){
+        return $this->belongsTo('C:\xampp\htdocs\sijapi\app\Models\Kelas_mahasiswa.php' , 'kode_kelas') ;
+    }
+
 }
