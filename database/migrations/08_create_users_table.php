d629babd7a4dc37142826a98d16a03ab2ad6facc
Moh Fatkhur Rahman <akufatkhur62@gmail.com>
2024-03-15 10:11:17 +0700

    ubah id di tabel users menjadi uuid

--- database/migrations/08_create_users_table.php
+++ database/migrations/08_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         
         Schema::create('users', function (Blueprint $table) {
-            $table->uuid('id')->primary();
+            $table->uuid('uuid')->primary();
             $table->string('username');
             $table->string('password');
             $table->enum('level', ['mahasiswa', 'dosen', 'admin']);
