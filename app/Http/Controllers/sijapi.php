3821eb61a664f17f00100b6d37f4ed333717e52b
RionWrath <jaya17.rj@gmail.com>
2024-03-25 15:07:51 +0700

    Messages

--- app/Http/Controllers/sijapi.php
+++ app/Http/Controllers/sijapi.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class sijapi extends Controller
 {
     public function index(){
-        return view('admin/page/admin');
+        return view('mahasiswa\page\beranda');
     }
 }
