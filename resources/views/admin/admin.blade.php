dd627cfe2c80703245145be70a13d420addeac77
Rais Hannan Rizanto <114925063+vierohanz@users.noreply.github.com>
2024-03-31 21:38:57 +0700

    modal

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -138,13 +138,13 @@
                         <span class="title ml-2">Password</span>
                     </a>
                 </li>
-
+                <hr class="mb-3 mt-7">
                 <li id="dashboard-nav">
                     <a href="{{ route('logout') }}" class="logout">
                         <span class="icon">
                             <svg class="ml-3" xmlns="http://www.w3.org/2000/svg" class="ionicon" width="30" height="54" viewBox="0 0 512 512"><path d="M304 336v40a40 40 0 01-40 40H104a40 40 0 01-40-40V136a40 40 0 0140-40h152c22.09 0 48 17.91 48 40v40M368 336l80-80-80-80M176 256h256" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/></svg>
                         </span>
-                        <span class="title ml-2">Sign Out</span>
+                        <span id="signOutButton" class="title ml-2 font-medium">Sign Out</span>
                     </a>
                 </li>
             </ul>
@@ -173,7 +173,6 @@
                         <span class="font-poppins font-semibold text-custom-birumuda">{{ isset($username) ? $username : 'Guest' }}</span>
                     </p>
                 </div>
-
             </div>
 
             
--- resources/views/admin/matkul.blade.php
+++ resources/views/admin/matkul.blade.php
@@ -88,7 +88,7 @@
                                     <td class="border px-4 py-2">3</td>
                                     <td class="border px-4 py-2">
                                         <button type="button" data-modal-target="edit_matkul_modal" data-modal-toggle="edit_matkul_modal" class="bg-blue-500 hover:bg-blue-700 text-white font-poppins font-normal py-1 px-2 rounded">Edit</button>
-                                        <div id="edit_matkul_modal" tabindex="-1" aria-hidden="true"class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                                        <div id="edit_matkul_modal" tabindex="-1" aria-hidden="true"class="hidden modal overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
                                             <div class="relative p-4 w-full max-w-md max-h-full">
 <!-- ==========================================================Modal content========================================================== -->
                                                 <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
@@ -172,7 +172,28 @@
         </div>
     </div>
 
-    
+    <script>
+        const modalToggle = document.querySelector("[data-modal-toggle]");
+const modal = document.getElementById("edit_matkul_modal");
+const overlay = document.createElement("div");
+overlay.classList.add("overlay");
+
+modalToggle.addEventListener("click", () => {
+    modal.classList.toggle("hidden");
+    modal.classList.toggle("flex");
+    document.body.appendChild(overlay);
+    overlay.style.display = "block";
+});
+
+const modalClose = document.querySelector("[data-modal-hide]");
+
+modalClose.addEventListener("click", () => {
+    modal.classList.toggle("hidden");
+    modal.classList.toggle("flex");
+    overlay.style.display = "none";
+    document.body.removeChild(overlay);
+});
+    </script>
 
 </body>
 
