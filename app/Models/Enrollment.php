605029c19613e0ef27395a36004872af411b54a5
fatkhurrahman23 <akufatkhur62@gmail.com>
2024-04-12 23:24:18 +0700

    benerin modelnya juga

--- app/Models/Enrollment.php
+++ app/Models/Enrollment.php
@@ -11,11 +11,11 @@ class Enrollment extends Model
 
     protected $table = 'enrollment';
     protected $primarykey = 'id';
-    protected $fillable = ['id', 'kode_enrollment', 'kode_kelas', 'kode_dosen', 'kode_tahun_akademik', 'kode_mata_kuliah']; 
+    protected $fillable = ['id', 'kode_enrollment', 'kode_dosen', 'kode_tahun_akademik', 'kode_mata_kuliah']; 
 
-    public function kelas_mahasiswa(){
-        return $this->belongsTo('app\Models\Kelas_mahasiswa.php', 'kode_kelas');
-    }
+    // public function kelas_mahasiswa(){
+    //     return $this->belongsTo('app\Models\Kelas_mahasiswa.php', 'kode_kelas');
+    // }
 
     public function mata_kuliah(){
         return $this->belongsTo('app\Models\Mata_kuliah.php', 'kode_enrollment');
